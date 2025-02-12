39b0874248c48f68b2146a71bc5ef172d0780fed
AndrewDem0 <144722259+AndrewDem0@users.noreply.github.com>
2025-02-12 17:37:31 +0200

    ---

--- WebApplication_lab/obj/Debug/net8.0/WebApplication_lab.AssemblyInfo.cs
+++ WebApplication_lab/obj/Debug/net8.0/WebApplication_lab.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication_lab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+269813fdbe0419c116ec5c0823958ed1cff9fdc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication_lab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication_lab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
