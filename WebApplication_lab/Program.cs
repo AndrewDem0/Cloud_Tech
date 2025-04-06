4e95389cb18d936336ab7f83368e6ae8d80844f9
AndrewDem0 <144722259+AndrewDem0@users.noreply.github.com>
2025-04-06 13:48:26 +0300

    lab5 console version

--- WebApplication_lab/Program.cs
+++ WebApplication_lab/Program.cs
@@ -1,37 +1,49 @@
-namespace WebApplication_lab
+using Azure;
+using System;
+using System.Globalization;
+using Azure.AI.TextAnalytics;
+
+namespace EntityLinkingExample
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	class Program
+	{
+		// This example requires environment variables named "LANGUAGE_KEY" and "LANGUAGE_ENDPOINT"
+		static string languageKey = Environment.GetEnvironmentVariable("LANGUAGE_KEY");
+		static string languageEndpoint = Environment.GetEnvironmentVariable("LANGUAGE_ENDPOINT");
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddApplicationInsightsTelemetry();
+		private static readonly AzureKeyCredential credentials = new AzureKeyCredential(languageKey);
+		private static readonly Uri endpoint = new Uri(languageEndpoint);
 
-            var app = builder.Build();
+		// Example method for recognizing entities and providing a link to an online data source.
+		static void EntityLinkingExample(TextAnalyticsClient client)
+		{
+			var response = client.RecognizeLinkedEntities(
+				"Microsoft was founded by Bill Gates and Paul Allen on April 4, 1975, " +
+				"to develop and sell BASIC interpreters for the Altair 8800. " +
+				"During his career at Microsoft, Gates held the positions of chairman, " +
+				"chief executive officer, president and chief software architect, " +
+				"while also being the largest individual shareholder until May 2014.");
+			Console.WriteLine("Linked Entities:");
+			foreach (var entity in response.Value)
+			{
+				Console.WriteLine($"\tName: {entity.Name},\tID: {entity.DataSourceEntityId},\tURL: {entity.Url}\tData Source: {entity.DataSource}");
+				Console.WriteLine("\tMatches:");
+				foreach (var match in entity.Matches)
+				{
+					Console.WriteLine($"\t\tText: {match.Text}");
+					Console.WriteLine($"\t\tScore: {match.ConfidenceScore:F2}\n");
+				}
+			}
+		}
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+		static void Main(string[] args)
+		{
+			var client = new TextAnalyticsClient(endpoint, credentials);
+			EntityLinkingExample(client);
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+			Console.Write("Press any key to exit.");
+			Console.ReadKey();
+		}
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
-        }
-    }
+	}
 }
