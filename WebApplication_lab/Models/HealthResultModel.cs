1dff9bd13b3a1bd0594165c241668cea5063f982
AndrewDem0 <144722259+AndrewDem0@users.noreply.github.com>
2025-04-09 14:03:40 +0300

    Lab8 del pii, heaslth

--- WebApplication_lab/Models/HealthResultModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication_lab.Models
-{
-    public class HealthResultModel
-    {
-        public List<string> Entities { get; set; }      // Список знайдених сутностей
-        public List<string> Categories { get; set; }    // Категорії сутностей
-        public List<int> ConfidenceScores { get; set; } // Відсоткова ймовірність для кожної сутності
-    }
-}
