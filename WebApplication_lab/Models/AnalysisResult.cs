f588b31237b0e8d2fac6bac325d5f3e204c6e180
AndrewDem0 <144722259+AndrewDem0@users.noreply.github.com>
2025-04-09 11:31:03 +0300

    Lab7 done

---
+++ WebApplication_lab/Models/AnalysisResult.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApplication_lab.Models
+{
+    public class AnalysisResult
+    {
+        public string OriginalText { get; set; }
+        public string ProcessedText { get; set; } // PII: ***, медичні дані: підкреслені
+        public List<EntityInfo> MedicalEntities { get; set; } = new List<EntityInfo>();
+        public List<PiiEntityInfo> HiddenPii { get; set; } = new List<PiiEntityInfo>();
+    }
+
+    public class EntityInfo
+    {
+        public string Text { get; set; }
+        public string Category { get; set; } // "Diagnosis", "Medication" тощо
+        public int Confidence { get; set; }  // 0-100%
+    }
+
+    public class PiiEntityInfo
+    {
+        public string Text { get; set; }     // Оригінальний текст (наприклад, "Іван Петренко")
+        public string Category { get; set; } // "Person", "Email" тощо
+        public int Confidence { get; set; }
+    }
+}
