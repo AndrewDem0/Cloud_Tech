b906427f833bc71bc544c2dbc8d9e670e1d28dc8
AndrewDem0 <144722259+AndrewDem0@users.noreply.github.com>
2025-04-10 10:53:07 +0300

    Lab9(1-2) done

---
+++ WebApplication_lab/Models/ImageAnalysisResult.cs
@@ -0,0 +1,14 @@
+﻿
+namespace WebApplication_lab.Models
+{
+    public class ImageAnalysisResult
+    {
+        public IFormFile ImageFile { get; set; }
+        public string Description { get; set; }
+        public float Confidence { get; set; }
+        public List<string> Tags { get; set; } = new List<string>();
+        public string ImageUrl { get; set; }
+
+
+    }
+}
---
+++ WebApplication_lab/Models/OcrResultModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication_lab.Models
+{
+    public class OcrResultModel
+    {
+        public IFormFile ImageFile { get; set; }
+        public string ImageUrl { get; set; }
+        public string ExtractedText { get; set; }
+        public double Confidence { get; set; } // Від 0 до 100
+    }
+}
