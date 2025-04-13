954e3186761721e853b934f4a1fe09a309354300
AndrewDem0 <144722259+AndrewDem0@users.noreply.github.com>
2025-04-13 16:36:40 +0300

    Lab9 done

---
+++ WebApplication_lab/Models/FaceDetectionResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace WebApplication_lab.Models
+{
+    public class FaceDetectionResult
+    {
+        public string ImageUrl { get; set; }
+        public int FaceCount { get; set; }
+        public List<FaceInfo> Faces { get; set; } = new();
+    }
+
+    public class FaceInfo
+    {
+        public int Left { get; set; }
+        public int Top { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public double Confidence { get; set; } // optional, use 1.0 for detection
+    }
+}
